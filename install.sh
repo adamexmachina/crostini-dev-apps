506a2d402021e4978989dcdd30bc4a212bd381c4
adam.macchi@cbn.org <be.kind.dont.spam@gmail.com>
2019-05-01 13:37:50 -0400

    Added more apps

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-#! /bin/bash
-#
 # Description:
 #   Installs web development tools in the Chromebook linux container.
 # Use:
@@ -11,6 +9,9 @@ main(){
 #   Compile a full-featured Vim from source on Ubuntu/Debian distros. Based
 #   entirely on
 #   https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source
+
+sudo apt update
+echo "y" | sudo apt upgrade
 
 echo "y" | sudo apt-get remove \
   vim \
@@ -40,7 +41,7 @@ echo "y" | sudo apt install \
   git
 
 cd ~
-hg clone https://github.com/vim/vim.git
+git clone https://github.com/vim/vim.git
 cd vim
 ./configure --with-features=huge \
   --enable-multibyte \
@@ -51,13 +52,56 @@ cd vim
   --enable-gui=gtk2 \
   --enable-cscope \
   --prefix=/usr/local
-  make VIMRUNTIMEDIR=/usr/share/vim/vim81
-  sudo make install
-  sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
-  sudo update-alternatives --set editor /usr/local/bin/vim
-  sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
-  sudo update-alternatives --set vi /usr/local/bin/vim
-  vim --version
+make VIMRUNTIMEDIR=/usr/share/vim/vim81
+sudo make install
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
+sudo update-alternatives --set editor /usr/local/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
+sudo update-alternatives --set vi /usr/local/bin/vim
+vim --version
+
+
+# Install Powerline Fonts
+sudo apt-get install fonts-powerline
+
+# Add vimrc
+cd ~
+wget https://raw.githubusercontent.com/adamexmachina/dotfiles/master/vimrc
+mv vimrc .vimrc
+
+# Open vim to install plugins
+vim
+
+# Install Zsh
+sudo apt install git-core zsh
+
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s $(which zsh) # Switch shell from bash to zsh
+
+# Install Chromium
+sudo apt install chromium
+
+# Install Firefox
+sudo apt install firefox-esr
+
+# Install Filezilla
+wget https://dl2.cdn.filezilla-project.org/client/FileZilla_3.41.2_i686-linux-gnu.tar.bz2
+tar -xjvf FileZilla_3.41.2_i686-linux-gnu.tar.bz2
+sudo rm -rf /opt/filezilla*
+sudo mv **FileZilla** /opt/**filezilla3**
+sudo ln -sf /opt/**filezilla3**/bin/filezilla /usr/bin/filezilla
+
+# Install GitKraken
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
 }
+
+# Install DBeaver
+wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
+sudo dpkg -i dbeaver-ce_latest_amd64.deb
+
+# Install Gnome Terminal
+sudo apt install gnome-terminal
 
 main

54cecc14ed3b0013552c3d3dcef566f8a482720d
adam.macchi@cbn.org <be.kind.dont.spam@gmail.com>
2019-05-02 17:02:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Description:
 #   Build and install all web development tools needed for the Chromebook Linux container
 # Use:
-#   sudo -E bash install.sh
+#   bash install.sh
 #
 set -x #echo on
 set expand-tilde off

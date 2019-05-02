f839f914d4e5afc02c4197c070798098af969246
adam.macchi@cbn.org <be.kind.dont.spam@gmail.com>
2019-05-02 14:21:05 -0400

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -1,4 +1,4 @@
-SCRIPT_PATH="${0%/*}"
+SCRIPT_PATH="${0%}"
 echo "y" | sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
   libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
   libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \

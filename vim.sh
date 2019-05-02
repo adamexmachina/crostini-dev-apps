8ea8e0d97d013258927997f022439987eacf32f9
adamexmachina <be.kind.dont.spam@gmail.com>
2019-05-02 02:06:52 +0000

    Fixed formatting

--- vim.sh
+++ vim.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 echo "y" | sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
-libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-python3-dev ruby-dev git build-dep vim-gtk
+  libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+  libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+  python3-dev ruby-dev git build-dep vim-gtk
 
 sudo apt remove vi vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-nox
 
@@ -14,14 +14,14 @@ git clone https://github.com/vim/vim.git
 cd $HOME/vim
 
 ./configure --with-features=huge \
-        --enable-multibyte \
-	    --enable-rubyinterp=yes \
-	    --enable-python3interp=yes \
-	    --with-python3-config-dir=/usr/lib/python3.5/config \
-	    --enable-perlinterp=yes \
-        --enable-gui=gtk2 \
-        --enable-cscope \
-        --prefix=/usr/local
+  --enable-multibyte \
+  --enable-rubyinterp=yes \
+  --enable-python3interp=yes \
+  --with-python3-config-dir=/usr/lib/python3.5/config \
+  --enable-perlinterp=yes \
+  --enable-gui=gtk2 \
+  --enable-cscope \
+  --prefix=/usr/local
 
 make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
 

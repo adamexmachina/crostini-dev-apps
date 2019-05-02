c076612c2a4c4121fec162dd33bb788ab69feea0
adamexmachina <be.kind.dont.spam@gmail.com>
2019-05-02 02:18:52 +0000

    Added symlink for vimrc and run vim immediately after to load plugins.

--- vim.sh
+++ vim.sh
@@ -37,4 +37,7 @@ sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
 sudo update-alternatives --set editor /usr/local/bin/vim
 sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
 sudo update-alternatives --set vi /usr/local/bin/vim
-vim --version
+
+ln -s $PWD/vimrc $HOME/.vimrc
+
+vim

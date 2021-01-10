336e89b830d40dd23bd52eb25632f45ca58afd29
AdamExMachina <be.kind.dont.spam@gmail.com>
2021-01-10 14:05:41 -0500

    Updated

--- install.sh
+++ install.sh
@@ -7,6 +7,12 @@
 #
 set -x #echo on
 
+# Update and Upgrade
+echo "y" | sudo apt update && sudo apt upgrade
+
+#Install Git
+echo "y" | sudo apt install git
+
 # Install Powerline Fonts
 echo "y" | sudo apt-get install fonts-powerline
 
@@ -18,11 +24,7 @@ sudo printf '[Desktop Entry]\nName=Chromium\nComment=Web Browser\nGenericName=We
 echo "y" | sudo apt install firefox-esr
 
 # Install Filezilla
-wget https://dl1.cdn.filezilla-project.org/client/FileZilla_3.41.2_x86_64-linux-gnu.tar.bz2?h=BFj_PnBcG8St2Y9mlX-XSQ&x=1556747020
-tar -xjvf FileZilla_3.41.2_x86_64-linux-gnu.tar.bz2
-sudo rm -rf /opt/filezilla*
-sudo mv ./**FileZilla** /opt/**filezilla3**
-sudo ln -sf /opt/**filezilla3**/bin/filezilla /usr/bin/filezilla
+echo "y" | sudo apt install filezilla
 
 # Install GitKraken
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
@@ -34,24 +36,8 @@ wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
 sudo dpkg -i dbeaver-ce_latest_amd64.deb
 echo "y" | sudo apt --fix-broken install
 
-# Install Gnome Terminal
-echo "y" | sudo apt install gnome-terminal
-sudo printf '[Desktop Entry]\nName=Gnome Terminal\nComment=Terminal Emulator\nGenericName=Terminal Emulator\nTryExec=gnome-terminal\nVersion=1.0\nExec=gnome-terminal\nMimeType=text/plain\nCategories=Utility;\nTerminal=false\nType=Application\nIcon=terminal\nStartupNotify=true\n' > $HOME/.local/share/applications/gnome-terminal.desktop
-
-# Install Apache
-echo "y" | sudo apt install apache2 apache2-utils
-sudo systemctl enable apache2
-sudo apache2 -v
-
-# Install MariaDB
-echo "y" | sudo apt install mariadb-server mariadb-client
-sudo systemctl enable mariadb
-sudo mysql_secure_installation
-
-# Install PHP
-echo "y" | sudo apt install php7.0 nano libapache2-mod-php7.0 php7.0-mysql php-common php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-readlin
-sudo a2enmod php7.0
-sudo systemctl restart apache2
+#Install Kitty Terminal Emulator
+echo "y" | sudo apt install kitty
 
 # Install Zsh
 echo "y" | sudo apt install git-core zsh
@@ -61,5 +47,42 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # sudo usermod -s "$(command -v zsh)" "${USER}"
 chsh -s "$(command -v zsh)" # Switch shell from bash to zsh
 
+#Install Vim
+echo "y" | sudo apt install vim
+
+# Install Docker
+sudo apt remove docker docker-engine docker.io containerd runc
+echo "y" | sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+echo "y" | sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+#Install Homebrew Linux
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+#Install DDEV Local
+brew tap drud/ddev && brew install ddev
+
 # Remove no longer required packages
 echo "y" | sudo apt autoremove
+
+
